b9da103e36e930aa502805a329dbeb94edbdc916
DomThaBomb <djamadotramondo1005@gmail.com>
2024-08-15 18:03:21 -0700

    Table Generator.js

---
+++ Table Generator.js
@@ -0,0 +1,31 @@
+function generateTable() {
+    // creates a <table> element and a <tbody> element
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+  
+    // creating all cells
+    for (let i = 1; i < 6; i++) {
+      // creates a table row
+      const row = document.createElement("tr");
+  
+      for (let j = 1; j < 6; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        const cell = document.createElement("td");
+        const cellText = document.createTextNode(`${i},${j}`);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+  
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+  
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    document.body.appendChild(tbl);
+    // sets the border attribute of tbl to '2'
+    tbl.setAttribute("border", "2");
+  }
